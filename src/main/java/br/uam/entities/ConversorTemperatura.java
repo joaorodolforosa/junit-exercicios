6f71c02e05a23a556b8a301457eb7d24e3db2978
Joao Rodolfo <joaorodolforosa@gmail.com>
2024-11-06 17:56:17 -0300

    Conversor de Temperatura

---
+++ src/main/java/br/uam/entities/ConversorTemperatura.java
@@ -0,0 +1,17 @@
+package br.uam.entities;
+
+/**
+ *
+ * @author Joao Rodolfo
+ */
+public class ConversorTemperatura {
+
+    public double celsiusParaFahrenheit(double celsius) {
+        return (celsius * 9/5) + 32;
+    }
+
+    public double fahrenheitParaCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5/9;
+    }
+}
+
